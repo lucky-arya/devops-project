96e4ce5459810de9b574231c4155c4d72664dcc8
Shivam Kumar <lklk112111@gmail.com>
2026-06-14 07:29:10 +0000

    Server Setup

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import app from './app.js'
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Listening On https://friendly-space-goldfish-q7p479jpqqp7c46jj-3000.app.github.dev/`);
+});
