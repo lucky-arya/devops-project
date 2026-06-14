b28a84fa701ae33a77685ad4f54b56f6c4cfa57c
Shivam Kumar <lklk112111@gmail.com>
2026-06-14 12:56:17 +0000

    update package.json with linting and formatting scripts; add devDependencies for ESLint and Prettier

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
-import app from './app.js'
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Listening On https://friendly-space-goldfish-q7p479jpqqp7c46jj-3000.app.github.dev/`);
+app.listen(PORT, () => {
+  console.log(
+    'Listening On https://friendly-space-goldfish-q7p479jpqqp7c46jj-3000.app.github.dev/'
+  );
 });
